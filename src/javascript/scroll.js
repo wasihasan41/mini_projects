b05b99ac6efecff3eb724194ec2714b4a04e14a4
wasihasan41 <wasihasan41@gmail.com>
2024-05-14 21:10:15 +0530

    asyncAwait

--- src/javascript/scroll.js
+++ src/javascript/scroll.js
@@ -1,55 +1,52 @@
 // document.designMode='on'; //can edit live on page
 //console.table(objName)  //showing object in table format
-function smoothScroll(target, duration){
-    var target= document.querySelector(target);
-    var targetPosition = target.getBoundingClientRect().top;
-    console.log(targetPosition);
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  console.log(targetPosition);
 }
-smoothScroll('.section1', 1000)
-
-
-
+smoothScroll(".section1", 1000);
 
 const student = {
-    age: 72,
-    func: function(){
-        return this.age;
-    }
-}
-console.log(student.func())
+  age: 72,
+  func: function () {
+    return this.age;
+  },
+};
+console.log(student.func());
 
 const obj1 = {
-    name: "obj1",
-    getThis(){
+  name: "obj1",
+  getThis() {
     return this;
-}
-}
+  },
+};
 const obj2 = {
-    name: "obj2"
-}
+  name: "obj2",
+};
 obj2.getThis = obj1.getThis;
-console.log(obj2.getThis)
+console.log(obj2.getThis);
 
-const sentence ={
-    firstWord: "hello",
-    lastWord: "world",
-    fullWord: function(){
-        return this.firstWord + " " + this.lastWord;
-    }
-}
+const sentence = {
+  firstWord: "hello",
+  lastWord: "world",
+  fullWord: function () {
+    return this.firstWord + " " + this.lastWord;
+  },
+};
 console.log(sentence.fullWord());
 
 const person1 = {
-    firstName: "wasi",
-    lastName: "hasan",
-}
+  firstName: "wasi",
+  lastName: "hasan",
+};
 
 const person2 = {
-    fullName: function(){
-        return this.firstName + " " + this.lastName;
-    }
-}
-const x =  person2.fullName.apply(person1);
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+const x = person2.fullName.apply(person1);
 console.log(x);
 
 // --- reverse ---
@@ -70,12 +67,12 @@ console.log(x);
 // console.log(fact);
 
 // --- fibonacci ---
-// let num = prompt("enter a number");                            
+// let num = prompt("enter a number");
 // let a = 0;
 // let b = 1;
 // console.log(a);
 // console.log(b);
-// for(let i = 3; i<=num; i++){                                                                                                                                                                                                                                                                                                                                                         
+// for(let i = 3; i<=num; i++){
 // fibo= a + b;
 // a=b;
 // b=fibo;
@@ -145,8 +142,6 @@ console.log(x);
 //     console.log("");
 // }
 
-
-
 // let promise =new Promise(function(resolve, reject){
 //     setTimeout(function(){
 //         console.log("hello in 2 sec");
@@ -157,23 +152,78 @@ console.log(x);
 //     console.log(value)
 // })
 
+// setTimeout(function () {
+//   console.log("3 second completed");
+// }, 3000);
 
+// setTimeout(function () {
+//   console.log("6 second completed");
+// }, 6000);
+// try {
+//   console.log(second);
+// } catch (e) {
+//   console.log(e);
+// }
+// setTimeout(function () {
+//   console.log("9 second completed");
+// }, 9000);
 
+// let num = 41;
+// try {
+//   if(num % 2 == 0){
+//     console.log(num)
+//   }
+// } catch (num) {
+//   console.log("invalid");
+// }
 
-setTimeout(function(){
-    console.log("3 second completed")
-},3000)
+// console.log("message")
+// logger(); //it won't go ahead until the function is finished
+// console.log(1);
 
-setTimeout(function(){
-    console.log("6 second completed")
-},6000)
-try{
-    console.log(second);
-}catch(e){
-    console.log(e)
+// function logger() {
+//   console.log(2);
+//   finish();
+//   console.log(3);
+//   function finish(){
+//     console.log("finished")
+//   }
+// }
+
+// function first(){
+//   console.log(1);
+// }
+// function second(){
+//   setTimeout(function(){
+//     console.log(2);
+//   }, 0)
+// }
+// function third(){
+//   console.log(3);
+// }
+// first();
+// second();
+// third();
+
+// const promise = new Promise(function (resolve, reject) {
+//   // resolve("we did it")
+//   reject("oops server down");
+// });
+// promise.then(function(Data){
+//   console.log(Data)
+// })
+// promise.catch(function (data) {
+//   console.log(data);
+// });
+
+async function getList() {
+  try {
+    const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    console.log(data);
+    const JSON = await data.json();
+    console.log(JSON);
+  } catch (err) {
+    console.log("please ensure u have established a internet conncetion");
+  }
 }
-setTimeout(function(){
-    console.log("9 second completed")
-},9000)
-
-
+getList();
