0f9ae366637adf1a606f5903c80767bd8b1a1f65
wasihasan41 <wasihasan41@gmail.com>
2024-11-16 21:10:03 +0530

    continue...

--- src/javascript/test.js
+++ src/javascript/test.js
@@ -5,22 +5,25 @@ const randomNumberButton = document.querySelector('button');
 
 randomNumberButton.addEventListener('click', getRandomNumber);
 function getRandomNumber(){
-  let random  = Math.floor(Math.random() * 6) + 1
-  randomNumber.textContent = random
+  
+  
+  let random  = Math.floor(Math.random() * 6) + 1;
+  let randomImage = `<img src="/assets/javascript/dice${random}.png" style="height:50px; width:50px;">`;
+  randomNumber.innerHTML = randomImage;
     for (let i = 0; i < box.length; i++) {
       const elem = box[i];
       
-      let activeNum = parseInt(active.dataset.number)
-      let dataNum = parseInt(elem.dataset.number) 
-      let newNum = (random + activeNum)
+      let activeNum = parseInt(active.dataset.number);
+      let dataNum = parseInt(elem.dataset.number); 
+      let newNum = (random + activeNum);
       if(newNum == dataNum){
-        active.classList.remove('color')
-        activeNum  =  newNum
-        active = document.querySelector(`[data-number="${activeNum}"]`)
+        active.classList.remove('color');
+        activeNum  =  newNum;
+        active = document.querySelector(`[data-number="${activeNum}"]`);
         console.log(active);
         active.classList.add('color');
       }
-      if(active.dataset.number == 16){
+      if(active.dataset.number == 36){
         randomNumberButton.removeEventListener('click', getRandomNumber);
         setTimeout(()=>{
           alert('won')
